3ffbe29d3de40394f9fa2f2b471ea17c6fa8c6f6
Brian Pontes <brian.robert16@hotmail.com>
2020-01-29 22:41:07 -0300

    Adicionando estrutura do projeto

---
+++ NerdStore/NerdStore/Src/NerdStore.Catalogo.Domain/Categoria.cs
@@ -0,0 +1,24 @@
+﻿using NerdStore.Core.DomainObjects;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.Catalogo.Domain
+{
+    public class Categoria : Entity
+    {
+        public Categoria(string nome, string codigo)
+        {
+            Nome = nome;
+            Codigo = codigo;
+        }
+
+        public override string ToString()
+        {
+            return $"{Nome} - {Codigo}";
+        }
+
+        public string Nome { get; private set; }
+        public string Codigo { get; private set; }
+    }
+}
